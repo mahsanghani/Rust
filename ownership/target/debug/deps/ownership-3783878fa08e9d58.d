8d8716ba3cc61b075e796ac4862a24d879d7d944
mahsanghani <ghaniahsan@outlook.com>
2022-01-10 04:23:26 -0800

    ownership

---
+++ ownership/target/debug/deps/ownership-3783878fa08e9d58.d
@@ -0,0 +1,5 @@
+/Users/ahsanghani/IdeaProjects/test_rust/ownership/target/debug/deps/ownership-3783878fa08e9d58: src/main.rs
+
+/Users/ahsanghani/IdeaProjects/test_rust/ownership/target/debug/deps/ownership-3783878fa08e9d58.d: src/main.rs
+
+src/main.rs:
---
+++ ownership/target/debug/ownership.d
@@ -0,0 +1,1 @@
+/Users/ahsanghani/IdeaProjects/test_rust/ownership/target/debug/ownership: /Users/ahsanghani/IdeaProjects/test_rust/ownership/src/main.rs
