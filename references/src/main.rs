b0a7afeef8e97e17aa0cf574993d2008210e7a45
mahsanghani <ghaniahsan@outlook.com>
2022-01-11 08:56:51 -0800

    references

--- references/src/main.rs
+++ references/src/main.rs
@@ -1,3 +1,47 @@
 fn main() {
-    println!("Hello, world!");
+    let s1 = String::from("hello");
+
+    {
+        let r1 = &mut s;
+    }
+
+    let r1 = &s; // no problem
+    let r2 = &s; // no problem
+    println!("{} and {}", r1, r2);
+    // variables r1 and r2 will not be used after this point
+
+    let r3 = &mut s; // no problem
+    println!("{}", r3);
+
+    // let r1 = &mut s;
+    // let r2 = &mut s;
+    // println!("{}, {}", r1, r2);
+
+    let len = calculate_length(&s1);
+
+    println!("The length of '{}' is {}.", s1, len);
+
+    change(&mut s);
+
+    let reference_to_nothing = dangle();
+}
+
+fn dangle() -> &String {
+    let s = String::from("hello");
+
+    &s
+}
+
+fn no_dangle() -> String {
+    let s = String::from("hello");
+
+    s
+}
+
+fn change(some_string: &mut String) {
+    some_string.push_str(", world");
+}
+
+fn calculate_length(s: &String) -> usize {
+    s.len()
 }
