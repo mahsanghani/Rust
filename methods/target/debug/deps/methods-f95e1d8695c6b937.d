26ed1afdb41f69a30fcbdff8f18a8f4344635ced
mahsanghani <ghaniahsan@outlook.com>
2022-02-13 08:37:09 -0800

    methods

---
+++ methods/target/debug/deps/methods-f95e1d8695c6b937.d
@@ -0,0 +1,5 @@
+/Users/ahsanghani/IdeaProjects/test_rust/methods/target/debug/deps/methods-f95e1d8695c6b937: src/main.rs
+
+/Users/ahsanghani/IdeaProjects/test_rust/methods/target/debug/deps/methods-f95e1d8695c6b937.d: src/main.rs
+
+src/main.rs:
---
+++ methods/target/debug/methods.d
@@ -0,0 +1,1 @@
+/Users/ahsanghani/IdeaProjects/test_rust/methods/target/debug/methods: /Users/ahsanghani/IdeaProjects/test_rust/methods/src/main.rs
