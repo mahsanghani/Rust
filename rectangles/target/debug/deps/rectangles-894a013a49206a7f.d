eec2be587b7700d207860e304bf0c8f9f460b569
mahsanghani <ghaniahsan@outlook.com>
2022-02-13 08:25:31 -0800

    rectangles

---
+++ rectangles/target/debug/deps/rectangles-894a013a49206a7f.d
@@ -0,0 +1,5 @@
+/Users/ahsanghani/IdeaProjects/test_rust/rectangles/target/debug/deps/rectangles-894a013a49206a7f: src/main.rs
+
+/Users/ahsanghani/IdeaProjects/test_rust/rectangles/target/debug/deps/rectangles-894a013a49206a7f.d: src/main.rs
+
+src/main.rs:
---
+++ rectangles/target/debug/rectangles.d
@@ -0,0 +1,1 @@
+/Users/ahsanghani/IdeaProjects/test_rust/rectangles/target/debug/rectangles: /Users/ahsanghani/IdeaProjects/test_rust/rectangles/src/main.rs
